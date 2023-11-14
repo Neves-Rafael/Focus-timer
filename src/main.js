1ea788a2d8d49c2f6236169940fb7af8f0a12ee8
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-11-14 18:08:27 -0300

    atualizado span

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ import "./switch-stations.js";
 import * as FocusTimer from "./FocusTimer/index.js";
 
 FocusTimer.start(25, 0);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+  }
+});
